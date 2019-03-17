ff27987919c9d7ec891eb4b1919034680e6ce82e
jurg96 <jurg96@gmail.com>
2019-03-17 13:23:29 +0300

    fixes

--- modules/PictureResolver.py
+++ modules/PictureResolver.py
@@ -1,32 +1,6 @@
 import numpy as np
 import skimage.util
-import scipy.signal
-
-
-def get_features(patch):
-    """
-    Extract the following features from the input patch:
-      - first-order gradients  [-1,0,1] in both directions
-      - second-order gradients [1,0,-2,0,1] in both directions
-      - first-order gradient magnitude
-      - second-order gradient magnitude
-    """
-    assert patch.ndim == 2
-    
-    k1 = np.array([[-1,0,1]])
-    k2 = k1.T
-    k3 = np.array([[1,0,-2,0,1]])
-    k4 = k3.T
-    
-    Fs = []
-    for k in [k1,k2,k3,k4]:
-        F = scipy.signal.convolve2d(patch, k, mode='same')
-        Fs.append(F)
-    Fs.append(np.sqrt(Fs[0]**2 + Fs[1]**2))
-    Fs.append(np.sqrt(Fs[2]**2 + Fs[3]**2))
-    Fs = np.dstack(Fs)
-    return Fs
-
+import utils
 
 class PictureResolver:
     # TODO: make size-informed and shape-agnostic
@@ -39,7 +13,7 @@ class PictureResolver:
     def resolve(self, img_in, patch_size=(9,9), step=6):
         assert img_in.ndim == 2
         
-        features = get_features(img_in)
+        features = utils.get_features(img_in)
         patches = self._split_into_patches(features, (*patch_size, 6), step=6)
         patches_arr_size = patches.shape[0:2]
 
@@ -59,7 +33,6 @@ class PictureResolver:
                 div_coef[step*i:step*i+p, step*j:step*j+p] += np.ones(patch_size)
         img_out_delta /= div_coef
         return img_in + img_out_delta
-    
     
     def _split_into_patches(self, img_array, patch_size, step=6):
         patches = skimage.util.view_as_windows(img_array, patch_size, step=step)
