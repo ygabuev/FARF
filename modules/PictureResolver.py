cea10d714f096e993900864426db645395d45ef4
jurg96 <jurg96@gmail.com>
2019-03-18 19:02:59 +0300

    evaluation

--- modules/PictureResolver.py
+++ modules/PictureResolver.py
@@ -6,9 +6,12 @@ class PictureResolver:
     # TODO: make size-informed and shape-agnostic
     # currently based on 9x9 patches
     
-    def __init__(self, rf, lsh):
+    def __init__(self, rf, lsh, scaler):
+        # rf - fitted random forest
+        # lsh - fitted lsh
         self.rf = rf
         self.lsh = lsh
+        self.scaler = scaler
     
     def resolve(self, img_in, patch_size=(9,9), step=6, augmented=True):
         assert img_in.ndim == 2
@@ -19,6 +22,7 @@ class PictureResolver:
         patches_arr_size = patches.shape[0:2]
 
         X = np.reshape(patches, (np.prod(patches_arr_size), -1))
+        X = self.scaler.transform(X)
         X_comp = self.lsh.transform(X)
         Y_pred = self.rf.predict(X, X_comp)                # MOST EXPENSIVE ACTION
         
--- utils/utils.py
+++ utils/utils.py
@@ -20,27 +20,30 @@ def resize(img_array, s=0.5, interpolation=Image.BICUBIC):
     size = (s * np.array(img_array.shape[0:2])).astype(np.int)
     return np.array(Image.fromarray(img_array).resize(size, interpolation))
 
-def get_features(patch, augmented=True):
-    """
-    Extract the following features from the input patch:
-      - first-order gradients  [-1,0,1] in both directions
-      - second-order gradients [1,0,-2,0,1] in both directions
-      - first-order gradient magnitude
-      - second-order gradient magnitude
-    """
-    assert patch.ndim == 2
-    
-    k1 = np.array([[-1,0,1]])
-    k2 = k1.T
-    k3 = np.array([[1,0,-2,0,1]])
-    k4 = k3.T
-    
-    Fs = []
-    for k in [k1,k2,k3,k4]:
-        F = scipy.signal.convolve2d(patch, k, mode='same')
-        Fs.append(F)
-    if augmented:
-        Fs.append(np.sqrt(Fs[0]**2 + Fs[1]**2))
-        Fs.append(np.sqrt(Fs[2]**2 + Fs[3]**2))
-    Fs = np.dstack(Fs)
-    return Fs
+
+def extract_features(img, augmented=True):
+        """
+        Extract the following features from the input image or patch:
+          - first-order gradients  [-1,0,1] in both directions
+          - second-order gradients [1,0,-2,0,1] in both directions
+          - first-order gradient magnitude
+          - second-order gradient magnitude
+          
+        Does not normalize augmented features
+        """
+        assert img.ndim == 2
+
+        k1 = np.array([[-1,0,1]])
+        k2 = k1.T
+        k3 = np.array([[1,0,-2,0,1]])
+        k4 = k3.T
+
+        Fs = []
+        for k in [k1,k2,k3,k4]:
+            F = scipy.signal.convolve2d(img, k, mode='same')
+            Fs.append(F)
+        if augmented:
+            Fs.append(np.sqrt(Fs[0]**2 + Fs[1]**2))
+            Fs.append(np.sqrt(Fs[2]**2 + Fs[3]**2))
+        Fs = np.dstack(Fs)
+        return Fs
